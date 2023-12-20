fdf7dc168bdc94b0f77b1fa6d8756cd1cce603a0
DorB2307 <dor.barnoach@post.runi.ac.il>
2023-12-20 09:12:41 +0200

    Add awesome code for Homework 1

--- Coins.java
+++ Coins.java
@@ -2,6 +2,6 @@ public class Coins {
     public static void main(String[] args) {
 	int cents = Integer.parseInt(args[0]);
 	int quarter = 25;
-        System.out.println("Use " + cents/quarter + " quarters and " + cents%quarter + " cents.");
+    System.out.println("Use " + cents/quarter + " quarters and " + cents%quarter + " cents.");
     }
 }
--- LinearEq.java
+++ LinearEq.java
@@ -1,8 +1,8 @@
 public class LinearEq {
     public static void main(String[] args) {
-	double a = Double.valueOf(args[0]);
-	double b = Double.valueOf(args[1]);
-	double c = Double.valueOf(args[2]);
+	double a = Double.parseDouble(args[0]);
+	double b = Double.parseDouble(args[1]);
+	double c = Double.parseDouble(args[2]);
 	double x = (c - b)/a;
     System.out.println(a + " * x + " + b + " = "+ c);
 	System.out.println("x = " + x);
