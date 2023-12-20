34180419d82be73f67ff85776cddd7177b733364
DorB2307 <dor.barnoach@post.runi.ac.il>
2023-12-20 08:22:49 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,9 +1,6 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
 public class AddTwo {
-	public static void main(String[] args) {
-		int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
-        	System.out.println(sum);	
-	}
+    public static void main(String[] args) {
+	int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+    System.out.println(sum);
+    }
 }
---
+++ Gen3.java
@@ -0,0 +1,16 @@
+public class Gen3 {
+   public static void main(String[] args) {
+        int min = Integer.parseInt(args[0]);
+        int max = Integer.parseInt(args[1]);
+        int range = max - min + 1;
+	int minRand = max;
+        for (int i = 0; i < 3; i++) {
+                int rand = (int)(Math.random() * range) + min;
+		System.out.println(rand);
+		if (minRand > rand) {
+			minRand = rand;
+		}
+	}
+	System.out.println("The minimal generated number was " + minRand);	
+   }
+} 
