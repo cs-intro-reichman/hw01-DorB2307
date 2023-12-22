7aa99233f25fbb3027c24106e78f9a376f598da2
DorB2307 <dor.barnoach@post.runi.ac.il>
2023-12-22 09:34:39 +0200

    I think there's something wrong with GenThreeTest, please check manually.

--- GenThree.java
+++ GenThree.java
@@ -3,14 +3,13 @@ public class GenThree {
         int min = Integer.parseInt(args[0]);
         int max = Integer.parseInt(args[1]);
         int range = max - min + 1;
-	int minRand = max;
-        for (int i = 0; i < 3; i++) {
-                int rand = (int)(Math.random() * range) + min;
-		System.out.println(rand);
-		if (minRand > rand) {
-			minRand = rand;
-		}
-	}
-	System.out.println("The minimal generated number was " + minRand);	
+        int num1 = (int)(Math.random() * range) + min;
+        int num2 = (int)(Math.random() * range) + min;
+        int num3 = (int)(Math.random() * range) + min;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        int minNum = Math.min(Math.min(num1, num2), num3);
+        System.out.println("The minimal generated number was " + minNum);	
    }
 } 
